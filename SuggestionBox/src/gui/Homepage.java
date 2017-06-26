5ad0c54de0daa10c026802acd8d56cd2db89f0b2
emerz35 <charlie.h.hands@gmail.com>
2017-06-26 21:00:32 +0100

    Added empty Homepage class

---
+++ SuggestionBox/src/gui/Homepage.java
@@ -0,0 +1,19 @@
+
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Charlie Hands
+ */
+public class Homepage {
+    
+    public Homepage(MainClass main){
+    
+    }
+    
+    public void display(JFrame frame){
+    
+    }
+}

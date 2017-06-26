60fb4236e1794634fd8588f411a4021d92d308a6
punk4eva <adam271828@gmail.com>
2017-06-26 15:15:20 +0100

    Update MainClass.java

--- SuggestionBox/src/gui/MainClass.java
+++ SuggestionBox/src/gui/MainClass.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.util.Random;
 
 import javax.swing.JFrame;
